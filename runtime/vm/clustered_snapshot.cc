527cbd5efa1eca70719b0e08379a8082002a94bb
Fabio Carneiro <fabio.carneiro@bmw.de>
2021-05-18 12:01:10 +0200

    Revert "remove snapshot error"
    This reverts commit 3e84e6729e5bba81a02662571a6ace3c5b065eae.

--- runtime/vm/clustered_snapshot.cc
+++ runtime/vm/clustered_snapshot.cc
@@ -6541,18 +6541,81 @@ char* SnapshotHeaderReader::VerifyVersion() {
   // If the version string doesn't match, return an error.
   // Note: New things are allocated only if we're going to return an error.
 
+  const char* expected_version = Version::SnapshotString();
+  ASSERT(expected_version != NULL);
+  const intptr_t version_len = strlen(expected_version);
+  if (stream_.PendingBytes() < version_len) {
+    const intptr_t kMessageBufferSize = 128;
+    char message_buffer[kMessageBufferSize];
+    Utils::SNPrint(message_buffer, kMessageBufferSize,
+                   "No full snapshot version found, expected '%s'",
+                   expected_version);
+    return BuildError(message_buffer);
+  }
+
+  const char* version =
+      reinterpret_cast<const char*>(stream_.AddressOfCurrentPosition());
+  ASSERT(version != NULL);
+  if (strncmp(version, expected_version, version_len) != 0) {
+    const intptr_t kMessageBufferSize = 256;
+    char message_buffer[kMessageBufferSize];
+    char* actual_version = Utils::StrNDup(version, version_len);
+    Utils::SNPrint(message_buffer, kMessageBufferSize,
+                   "Wrong %s snapshot version, expected '%s' found '%s'",
+                   (Snapshot::IsFull(kind_)) ? "full" : "script",
+                   expected_version, actual_version);
+    free(actual_version);
+    return BuildError(message_buffer);
+  }
+  stream_.Advance(version_len);
 
   return nullptr;
 }
 
 char* SnapshotHeaderReader::VerifyFeatures(Isolate* isolate) {
+  const char* expected_features =
+      Dart::FeaturesString(isolate, (isolate == NULL), kind_);
+  ASSERT(expected_features != NULL);
+  const intptr_t expected_len = strlen(expected_features);
 
+  const char* features = nullptr;
+  intptr_t features_length = 0;
+
+  auto error = ReadFeatures(&features, &features_length);
+  if (error != nullptr) {
+    return error;
+  }
+
+  if (features_length != expected_len ||
+      (strncmp(features, expected_features, expected_len) != 0)) {
+    const intptr_t kMessageBufferSize = 1024;
+    char message_buffer[kMessageBufferSize];
+    char* actual_features = Utils::StrNDup(
+        features, features_length < 1024 ? features_length : 1024);
+    Utils::SNPrint(message_buffer, kMessageBufferSize,
+                   "Snapshot not compatible with the current VM configuration: "
+                   "the snapshot requires '%s' but the VM has '%s'",
+                   actual_features, expected_features);
+    free(const_cast<char*>(expected_features));
+    free(actual_features);
+    return BuildError(message_buffer);
+  }
+  free(const_cast<char*>(expected_features));
   return nullptr;
 }
 
 char* SnapshotHeaderReader::ReadFeatures(const char** features,
                                          intptr_t* features_length) {
-
+  const char* cursor =
+      reinterpret_cast<const char*>(stream_.AddressOfCurrentPosition());
+  const intptr_t length = Utils::StrNLen(cursor, stream_.PendingBytes());
+  if (length == stream_.PendingBytes()) {
+    return BuildError(
+        "The features string in the snapshot was not '\\0'-terminated.");
+  }
+  *features = cursor;
+  *features_length = length;
+  stream_.Advance(length + 1);
   return nullptr;
 }
 
